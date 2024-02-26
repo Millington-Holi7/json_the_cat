e2f2e863efde03b5b5da23219452c4e1caef2cf6
Millington-Holi7 <holinas007@gmail.com>
2024-02-26 15:54:12 -0500

    Added test code and refractored.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,23 @@
 const request = require('request');
-const breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => { // request body from the url
-  if (error) {
-    console.log("URL inputted is not valid.");
-  } else {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('typeof before parse:', typeof body);
-    //console.log('body:', body); // Print the HTML body
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => { // request body from the url
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
     const data = JSON.parse(body); // body becomes an object
-    
     if (!data.length) { // info is in an array so we check the length to see if their is any info in it
-      console.log("Breed info does not exist!");
-      
-    } else {
-      console.log(data);
-      const x = data[0].description;
-      console.log(x);
-      console.log('typeof: ', typeof data);
+      callback("Breed info does not exist!", null);
+      return;
     }
-  }
-});
+
+    const x = data[0].description;
+    callback(null, x);
+  });
+};
+
+module.exports = { fetchBreedDescription };
